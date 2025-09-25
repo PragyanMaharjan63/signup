4ad18c35e6adc39a833bf8d53d8edf536101bedd
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-09-25 21:08:47 +0545

    adding popup for signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import SignUp from "./assets/Signup";
+
 function App() {
   return (
     <>
       <div className=" grid justify-items-center h-screen items-center">
-        <div className="ring-1 px-6 py-3 rounded-lg font-semibold transition-all duraiton-300 ease-in-out ring-blue-600 bg-blue-400/20 cursor-pointer bg-gradient-to-r hover:from-blue-700 hover:to-blue-500 hover:text-white hover:-translate-y-0.5 hover:shadow-lg">
+        <div
+          className="
+        row-start-1 col-start-1
+        ring-1 px-6 py-3 rounded-lg font-semibold transition-all duraiton-300 ease-in-out ring-blue-600 cursor-pointer bg-gradient-to-r from-blue-600 to-blue-500 hover:from-blue-700 hover:to-blue-500 text-white hover:-translate-y-0.5 hover:shadow-lg"
+        >
           Sign in
+        </div>
+        <div className="row-start-1 col-start-1">
+          <SignUp />
         </div>
       </div>
     </>
---
+++ frontend/src/assets/Signup.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <>
+      <div className="bg-white rounded-lg p-4 ring-1 ring-blue-500">SIGNUP</div>
+    </>
+  );
+}
