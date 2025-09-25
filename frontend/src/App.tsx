edad34aee2e72cef4b5c0338b01bbce58a1f011e
PragyanMaharjan63 <pragyanmaharjan6k@gmail.com>
2025-09-25 20:15:54 +0545

    initializing stuffs

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" grid justify-items-center h-screen items-center">
+        hello world
+      </div>
+    </>
+  );
+}
+
+export default App;
