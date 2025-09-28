c947474e5a8036b9b70c6c9356913b8dd13fad9b
pragyanmaharjan63 <pragyanmaharjan6k@gmail.com>
2025-09-28 17:01:02 +0545

    added dot env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
-const PORT = 3000;
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT;
 
 app.post("/auth/signup", (req, res) => {
   console.log("Hello world");
